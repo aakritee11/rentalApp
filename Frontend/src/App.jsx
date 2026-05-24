df300597e6211bd4e1da6cd4dc7c3f5a96ffff22
Aakriti <bomzanaakriti@gmail.com>
2026-05-24 23:02:07 +0545

    Setup pages and components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import Home from './components/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <>
-      <Home/>
+     
+      <Navbar/>
       <div>
         <h2>
           RentalApp
