7e4a1ce9fb67f3f1e8d773138c886c27eea913a9
Aakriti <bomzanaakriti@gmail.com>
2026-05-30 16:36:10 +0545

    Added RoomCard component with style

--- Frontend/src/components/RoomCard.jsx
+++ Frontend/src/components/RoomCard.jsx
@@ -1,10 +1,20 @@
+import '../styles/RoomCard.css';
 
-
-export default function RoomCard (){
+export default function RoomCard ({room, onClick}){
     return(
-        <div>
-            <div>
-                <h2>RoomCard</h2>
+        <div className="room-card" onClick={onClick}>
+            <div className="room-image">
+                {room.photos?.length >0? (
+                    <img src={room.photos[0]} alt={room.title} />
+                ): (
+                    <div className="no-image">No Image</div>
+                )}
+            </div>
+            <div className="room-info">
+                <h3>{room.title}</h3>
+                <p className="room-city ">{room.city}</p>
+                <p className="room-type">{room.type}</p>
+                <p className="room-price">Rs. {room.price}/month</p>
             </div>
         </div>
     )
